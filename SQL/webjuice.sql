c91752db8bddb243e9e5ac8efb270c937596c4e5
ValGou <val.gougeon@hotmail.fr>
2016-02-25 18:11:55 +0100

    BDD en anglais + exemples

--- SQL/webjuice.sql
+++ SQL/webjuice.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 08 Février 2016 à 22:59
+-- Généré le :  Jeu 25 Février 2016 à 18:10
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -23,89 +23,122 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `domaine`
+-- Structure de la table `area`
 --
 
-CREATE TABLE IF NOT EXISTS `domaine` (
+CREATE TABLE IF NOT EXISTS `area` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `area`
+--
+
+INSERT INTO `area` (`id`, `name`) VALUES
+(1, 'Marketing');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `entreprise`
+-- Structure de la table `company`
 --
 
-CREATE TABLE IF NOT EXISTS `entreprise` (
+CREATE TABLE IF NOT EXISTS `company` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
   `mail` varchar(128) NOT NULL,
-  `mdp` varchar(64) NOT NULL,
-  `pays` enum('France','Irlande') NOT NULL,
-  `ville` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `country` enum('France','Irlande') NOT NULL,
+  `city` varchar(64) NOT NULL,
   `desc` text NOT NULL,
   `website` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `company`
+--
+
+INSERT INTO `company` (`id`, `name`, `mail`, `password`, `country`, `city`, `desc`, `website`) VALUES
+(1, 'BDifferent', 'hello@bdifferent.ie', 'ptut', 'Irlande', 'Dublin', 'Une agence de marketing digital. Oui oui, c''est une vraie !', 'http://www.bdifferent.ie/');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `etudiant`
+-- Structure de la table `internship`
 --
 
-CREATE TABLE IF NOT EXISTS `etudiant` (
+CREATE TABLE IF NOT EXISTS `internship` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(64) NOT NULL,
-  `prenom` varchar(64) NOT NULL,
-  `pays` enum('France','Irlande') NOT NULL,
-  `domaine` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `desc` text NOT NULL,
+  `company` int(11) NOT NULL,
+  `way_num` int(128) NOT NULL,
+  `way_type` enum('Allée','Avenue','Boulevard','Carrrefour','Chemin','Chaussée','Cité','Corniche','Cours','Domaine','Descente','Ecart','Esplanade','Faubourg','Grande Rue','Hameau','Halle','Impasse','Lieu-dit','Lotissement','Marché','Montée','Passage','Place','Plaine','Plateau','Promenade','Parvis','Quartier','Quai','Résidence','Ruelle','Rocade','Rond-point','Route','Rue','Sente','Sentier','Square','Terre-plein','Traverse','Villa','Village') NOT NULL,
+  `way_name` varchar(64) NOT NULL,
+  `city` varchar(128) NOT NULL,
+  `zip_code` int(11) NOT NULL,
+  `area` int(11) NOT NULL,
+  `student` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `company` (`company`,`area`,`student`),
+  KEY `area` (`area`),
+  KEY `student` (`student`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `internship`
+--
+
+INSERT INTO `internship` (`id`, `name`, `desc`, `company`, `way_num`, `way_type`, `way_name`, `city`, `zip_code`, `area`, `student`) VALUES
+(1, 'Community manager', 'Notre agence cherche un stagiaire pour remplir le rôle de community manager.', 1, 5, 'Avenue', 'Gowna Plaza', 'Dublin', 15, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(64) NOT NULL,
+  `last_name` varchar(64) NOT NULL,
+  `country` enum('France','Irlande') NOT NULL,
+  `area` int(11) NOT NULL,
   `mail` varchar(64) NOT NULL,
-  `mdp` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
   `cv` varchar(128) NOT NULL,
   `portfolio` varchar(128) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `domaine` (`domaine`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `area` (`area`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Structure de la table `stage`
+-- Contenu de la table `student`
 --
 
-CREATE TABLE IF NOT EXISTS `stage` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(64) NOT NULL,
-  `desc` text NOT NULL,
-  `entreprise` int(11) NOT NULL,
-  `domaine` int(11) NOT NULL,
-  `etudiant` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `entreprise` (`entreprise`,`domaine`,`etudiant`),
-  KEY `domaine` (`domaine`),
-  KEY `etudiant` (`etudiant`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `country`, `area`, `mail`, `password`, `cv`, `portfolio`) VALUES
+(1, 'Valentin', 'Gougeon', 'France', 1, 'val.gougeon@hotmail.fr', 'ptut', '', '');
 
 --
 -- Contraintes pour les tables exportées
 --
 
 --
--- Contraintes pour la table `etudiant`
+-- Contraintes pour la table `internship`
 --
-ALTER TABLE `etudiant`
-  ADD CONSTRAINT `etudiant_ibfk_1` FOREIGN KEY (`domaine`) REFERENCES `domaine` (`id`);
+ALTER TABLE `internship`
+  ADD CONSTRAINT `internship_ibfk_3` FOREIGN KEY (`student`) REFERENCES `student` (`id`),
+  ADD CONSTRAINT `internship_ibfk_1` FOREIGN KEY (`company`) REFERENCES `company` (`id`),
+  ADD CONSTRAINT `internship_ibfk_2` FOREIGN KEY (`area`) REFERENCES `area` (`id`);
 
 --
--- Contraintes pour la table `stage`
+-- Contraintes pour la table `student`
 --
-ALTER TABLE `stage`
-  ADD CONSTRAINT `stage_ibfk_3` FOREIGN KEY (`etudiant`) REFERENCES `etudiant` (`id`),
-  ADD CONSTRAINT `stage_ibfk_1` FOREIGN KEY (`entreprise`) REFERENCES `entreprise` (`id`),
-  ADD CONSTRAINT `stage_ibfk_2` FOREIGN KEY (`domaine`) REFERENCES `domaine` (`id`);
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`area`) REFERENCES `area` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
