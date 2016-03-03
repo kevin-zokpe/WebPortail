3a9f88e1677472547508fd9382e060245705573f
Ange-Kevin <kzokpe@gmail.com>
2016-03-03 16:11:59 +0100

    Update stages.sql
    AK BDD LOGIN

--- SQL/stages.sql
+++ SQL/stages.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.2.10
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Jeu 03 Mars 2016 à 15:20
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Client :  localhost:8889
+-- Généré le :  Jeu 03 Mars 2016 à 16:09
+-- Version du serveur :  5.5.38
+-- Version de PHP :  5.6.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `stages`
@@ -23,73 +17,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `area`
---
-
-CREATE TABLE IF NOT EXISTS `area` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
-
---
--- Contenu de la table `area`
---
-
-INSERT INTO `area` (`id`, `name`) VALUES
-(1, 'Marketing'),
-(2, 'Sport'),
-(3, 'Commerce et Gestion'),
-(4, 'Droit social'),
-(5, 'Art et Design'),
-(6, 'Santé'),
-(7, 'Science Environnementale'),
-(8, 'Développement Informatique'),
-(9, 'Réseaux et Télécommunication'),
-(10, 'Aérospatial / Aéronautique'),
-(11, 'Mécanique et Électronique'),
-(12, 'Architecture et Bâtiment'),
-(13, 'Web et Communication'),
-(14, 'Audiovisuel et Animation'),
-(15, 'Énergie thermique et électrique');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `company`
 --
 
-CREATE TABLE IF NOT EXISTS `company` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `company` (
+`id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
-  `mail` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
   `password` varchar(64) NOT NULL,
   `country` enum('France','Irlande') NOT NULL,
   `city` varchar(64) NOT NULL,
-  `desc` text NOT NULL,
-  `website` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `description` text NOT NULL,
+  `website` varchar(64) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `company`
 --
 
-INSERT INTO `company` (`id`, `name`, `mail`, `password`, `country`, `city`, `desc`, `website`) VALUES
-(1, 'BDifferent', 'hello@bdifferent.ie', 'ptut', 'Irlande', 'Dublin', 'Une agence de marketing digital. Oui oui, c''est une vraie !', 'http://www.bdifferent.ie/');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `faq`
---
-
-CREATE TABLE IF NOT EXISTS `faq` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `question` text NOT NULL,
-  `answer` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+INSERT INTO `company` (`id`, `name`, `email`, `password`, `country`, `city`, `description`, `website`) VALUES
+(1, 'BDifferent', 'hello@bdifferent.ie', '$2y$12$6FBWV3.XhBB5vc7.1HayAunpLopRdrJJQbAEIcDLDvU/TOd4MJaGm', 'Irlande', 'Dublin', 'Une agence de marketing digital. Oui oui, c''est une vraie !', 'http://www.bdifferent.ie/'),
+(2, 'Hadrien Design', 'support@hadriendesign.com', '$2y$12$qfiWQpEP8UAwoSyZIMxnzeDCI6f5KVJVoReUuh8Al/YKZ2RZ81s2q', 'France', 'Rennes', 'Hadrien Design est une entreprise qui se charge de regrouper des développeurs et designers pour mettre en commun leur expérience.', 'https://hadriendesign.com/');
 
 -- --------------------------------------------------------
 
@@ -97,28 +45,46 @@ CREATE TABLE IF NOT EXISTS `faq` (
 -- Structure de la table `internship`
 --
 
-CREATE TABLE IF NOT EXISTS `internship` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `internship` (
+`id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
-  `desc` text NOT NULL,
+  `description` text NOT NULL,
   `company` int(11) NOT NULL,
-  `address` varchar(128) NOT NULL,
+  `way_num` int(128) NOT NULL,
+  `way_type` varchar(64) NOT NULL,
+  `way_name` varchar(64) NOT NULL,
   `city` varchar(128) NOT NULL,
   `zip_code` varchar(32) NOT NULL,
-  `area` int(11) NOT NULL,
-  `student` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `company` (`company`,`area`,`student`),
-  KEY `area` (`area`),
-  KEY `student` (`student`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `skill` int(11) NOT NULL,
+  `student` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `internship`
 --
 
-INSERT INTO `internship` (`id`, `name`, `desc`, `company`, `address`, `city`, `zip_code`, `area`, `student`) VALUES
-(1, 'Community manager', 'Notre agence cherche un stagiaire pour remplir le rôle de community manager.', 1, 'Gowna Plaza', 'Dublin', '15', 1, 1);
+INSERT INTO `internship` (`id`, `name`, `description`, `company`, `way_num`, `way_type`, `way_name`, `city`, `zip_code`, `skill`, `student`) VALUES
+(1, 'Community manager', 'Notre agence cherche un stagiaire pour remplir le rôle de community manager.', 1, 5, 'Avenue', 'Gowna Plaza', 'Dublin', '15', 1, 1),
+(3, 'Développeur Symfony2', 'Afin d''améliorer le site et son fonctionnement, nous recherchons un stagiaire capable de migrer notre site actuel vers le framework Symfony2.', 2, 16, 'Rue', 'Jean Monnet', 'Rennes', '35200', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `skill`
+--
+
+CREATE TABLE `skill` (
+`id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `skill`
+--
+
+INSERT INTO `skill` (`id`, `name`) VALUES
+(1, 'Marketing'),
+(2, 'Programmation');
 
 -- --------------------------------------------------------
 
@@ -126,29 +92,79 @@ INSERT INTO `internship` (`id`, `name`, `desc`, `company`, `address`, `city`, `z
 -- Structure de la table `student`
 --
 
-CREATE TABLE IF NOT EXISTS `student` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `student` (
+`id` int(11) NOT NULL,
   `first_name` varchar(64) NOT NULL,
   `last_name` varchar(64) NOT NULL,
   `country` enum('France','Irlande') NOT NULL,
-  `area` int(11) NOT NULL,
+  `skill` int(11) NOT NULL,
   `email` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
   `cv` varchar(128) NOT NULL,
-  `portfolio` varchar(128) NOT NULL,
-  `admin` tinyint(1) NOT NULL DEFAULT '0',
-  `available` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `area` (`area`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `portfolio` varchar(128) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `student`
 --
 
-INSERT INTO `student` (`id`, `first_name`, `last_name`, `country`, `area`, `email`, `password`, `cv`, `portfolio`, `admin`, `available`) VALUES
-(1, 'Valentin', 'Gougeon', 'France', 1, 'val.gougeon@hotmail.fr', 'ptut', '', '', 0, 1);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `country`, `skill`, `email`, `password`, `cv`, `portfolio`) VALUES
+(1, 'Valentin', 'Gougeon', 'France', 1, 'val.gougeon@hotmail.fr', '$2y$12$CyMjfQyBElBAq0wxa3J3j.Txoxza/dwWS.C43RCzqSG7dDR.Qolvu', '', ''),
+(2, 'Hadrien', 'Rannou', 'France', 1, 'hadriien@live.fr', '$2y$12$iG3tJq9C3iH1OzFqa2s0ruSa1zblb4XlF2Q0uZtvbW1KXy7KI/zW6', '', ''),
+(3, 'Ange-Kévin', 'Zokpe', 'France', 2, 'kzokpe@gmail.com', '$2y$12$PHQVWOZuCfgK3h2tYpge0uAD3DNEh9GkKSCyn4iygS8fVY7Hbmtdy', '', '');
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `company`
+--
+ALTER TABLE `company`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `internship`
+--
+ALTER TABLE `internship`
+ ADD PRIMARY KEY (`id`), ADD KEY `company` (`company`,`skill`,`student`), ADD KEY `skill` (`skill`), ADD KEY `student` (`student`);
+
+--
+-- Index pour la table `skill`
+--
+ALTER TABLE `skill`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `student`
+--
+ALTER TABLE `student`
+ ADD PRIMARY KEY (`id`), ADD KEY `skill` (`skill`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `company`
+--
+ALTER TABLE `company`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `internship`
+--
+ALTER TABLE `internship`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `skill`
+--
+ALTER TABLE `skill`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `student`
+--
+ALTER TABLE `student`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Contraintes pour les tables exportées
 --
@@ -157,16 +173,12 @@ INSERT INTO `student` (`id`, `first_name`, `last_name`, `country`, `area`, `emai
 -- Contraintes pour la table `internship`
 --
 ALTER TABLE `internship`
-  ADD CONSTRAINT `internship_ibfk_1` FOREIGN KEY (`company`) REFERENCES `company` (`id`),
-  ADD CONSTRAINT `internship_ibfk_2` FOREIGN KEY (`area`) REFERENCES `area` (`id`),
-  ADD CONSTRAINT `internship_ibfk_3` FOREIGN KEY (`student`) REFERENCES `student` (`id`);
+ADD CONSTRAINT `internship_ibfk_1` FOREIGN KEY (`company`) REFERENCES `company` (`id`),
+ADD CONSTRAINT `internship_ibfk_2` FOREIGN KEY (`skill`) REFERENCES `skill` (`id`),
+ADD CONSTRAINT `internship_ibfk_3` FOREIGN KEY (`student`) REFERENCES `student` (`id`);
 
 --
 -- Contraintes pour la table `student`
 --
 ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`area`) REFERENCES `area` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`skill`) REFERENCES `skill` (`id`);
