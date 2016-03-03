9f6550ab9a61e46c148a1fdbcd8c0dbc8d719349
Hadrien <hadriien@live.fr>
2016-03-03 15:02:31 +0100

    Update home.php
    Modification légère de l'apparance

--- PHP/app/view/home.php
+++ PHP/app/view/home.php
@@ -1,15 +1,17 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-8">
-			<h1>Web Portal</h1>
-			<?php
-				if (!App::isLogged()) {
-			?>
-			<p>Me connecter en tant que :</p>
-			<ul>
-				<li><a href="index.php?page=login&amp;type=student">Étudiant</a></li>
-				<li><a href="index.php?page=login&amp;type=company">Entreprise</a></li>
-			</ul>
+		<div class="col-md-12">
+			<div class="jumbotron" style="text-align: center;">
+				<h1>Web Portal</h1>
+				<?php
+					if (!App::isLogged()) {
+				?>
+				<p>Me connecter en tant que :</p>
+				<p>
+					<a class="btn btn-primary btn-lg" href="index.php?page=login&amp;type=student" role="button">Étudiant</a>
+					<a class="btn btn-primary btn-lg" href="index.php?page=login&amp;type=company" role="button">Entreprise</a>
+				</p>
+			</div>
 			<?php
 				}
 
