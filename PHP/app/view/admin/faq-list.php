6e84caa23c2bd6f8e8c0d8c1927227cb6dafb4c8
kzokpe <kzokpe@gmail.com>
2016-03-09 19:40:25 +0100

    Suppression bugs

--- PHP/app/view/admin/faq-list.php
+++ PHP/app/view/admin/faq-list.php
@@ -59,8 +59,5 @@
 		?>
 	</table>
 	<a href="index.php?page=admin/add-faq&amp;type=<?php echo $type; ?>" class="btn btn-primary">Ajouter une question</a>
-<<<<<<< HEAD
 </div>
-=======
 </div>
->>>>>>> origin/master
