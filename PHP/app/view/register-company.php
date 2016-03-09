fbe84c4c2f148da3760384ddbaa9f6d243522f45
Hadrien <hadriien@live.fr>
2016-03-09 23:42:01 +0100

    Update register-company.php
    Ajout du recaptcha

--- PHP/app/view/register-company.php
+++ PHP/app/view/register-company.php
@@ -1,16 +1,19 @@
 <?php
 	if (isset($_POST['register'])) {
-		if (isset($_POST['name']) && $_POST['name']!="" && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name']) &&
-    		isset($_POST['email']) && $_POST['email']!="" && preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email']) &&
-    		isset($_POST['email-confirm']) && $_POST['email-confirm']==$_POST['email'] &&
-    	   	isset($_POST['password']) && $_POST['password']!="" && preg_match("#^[a-zA-Z\@._-]{2,32}#", $_POST['password']) &&
-    	   	isset($_POST['password-confirm']) && $_POST['password-confirm']==$_POST['password'] &&
-    	   	isset($_POST['country'] ) && $_POST['country']!="" &&
-    	   	isset($_POST['city']) && $_POST['city']!="" && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['city']) &&
-    	   	isset($_POST['description']) && $_POST['description']!="" && preg_match("#^[a-zA-Z0-9._-]{2,128}#", $_POST['description']) &&
-    	   	isset($_POST['website']) && $_POST['website']!="" && 
+		if (isset($_POST['name']) && !empty($_POST['name']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name']) &&
+    		isset($_POST['email']) && !empty($_POST['email']) && preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email']) &&
+    		isset($_POST['email-confirm']) && $_POST['email-confirm'] == $_POST['email'] &&
+    	   	isset($_POST['password']) && !empty($_POST['password']) && preg_match("#^[a-zA-Z\@._-]{2,32}#", $_POST['password']) &&
+    	   	isset($_POST['password-confirm']) && $_POST['password-confirm'] == $_POST['password'] &&
+    	   	isset($_POST['country'] ) && !empty($_POST['country']) &&
+    	   	isset($_POST['city']) && !empty($_POST['city']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['city']) &&
+    	   	/*isset($_FILES['cv']) &&*/
+    	   	isset($_POST['description']) && !empty($_POST['description']) && preg_match("#^[a-zA-Z0-9._-]{2,128}#", $_POST['description']) &&
+    	   	isset($_POST['website']) && !empty($_POST['website']) && 
     	   		preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['website']) && 
-    	   	isset($_POST['accept_terms'])) {
+    	   	isset($_POST['accept_terms']) &&
+    	   	isset($_POST['g-recaptcha-response'])
+    	   ) {
     	    
 			$name = $_POST['name'];
 			$city = $_POST['city'];
@@ -20,13 +23,13 @@
 			$description = $_POST['description'];
 			$website = $_POST['website'];
     	    
-    	    
 			try {
 				PDOConnexion::setParameters('stages', 'root', 'root');
 				$db = PDOConnexion::getInstance();
 				$sql = "
 					INSERT INTO company(name, email, country, city, description, password, website, activated, register_date)
-					VALUES (:name, :email, :country, :city, :description, :password, :website, false, NOW())";
+					VALUES (:name, :email, :country, :city, :description, :password, :website, false, NOW())
+				";
 				$sth = $db->prepare($sql);
 				$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Student');
 				$sth->execute(array(
@@ -39,50 +42,62 @@
 					':website' => $website
 				));
 
+				/*if($_FILES['cv']){
+    				echo "FILE UPLOADED!";
+				}*/
+
 				App::redirect('Location: index.php?page=home');
 			}
 
-			catch(PDOException$e){
-				echo"<p>Erreur:".$e->getMessage()."</p>";
+			catch(PDOException$e) {
+				echo '<p>Erreur : ' . $e->getMessage() . '</p>';
 				die();
 			}
 		}
 
-		else{
-			if((!isset($_POST['name']) || $_POST['name']=="") || 
-			(!isset($_POST['city']) || $_POST['city']=="") ||
-			(!isset($_POST['email']) || $_POST['email']=="") ||
-			(!isset($_POST['email-confirm']) || $_POST['email-confirm']=="") ||
-			(!isset($_POST['password']) || $_POST['password']=="") ||
-			(!isset($_POST['password-confirm']) || $_POST['password-confirm']=="") ||
-			(!isset($_POST['country']) || $_POST['country']=="") ||
-			(!isset($_POST['description']) || $_POST['description']=="") ||
-			(!isset($_POST['website']) || $_POST['website']=="")){
+		else {
+			if ((!isset($_POST['name']) || empty($_POST['name'])) || 
+			(!isset($_POST['city']) || empty($_POST['city'])) ||
+			(!isset($_POST['email']) || empty($_POST['email'])) ||
+			(!isset($_POST['email-confirm']) || empty($_POST['email-confirm'])) ||
+			(!isset($_POST['password']) || empty($_POST['password'])) ||
+			(!isset($_POST['password-confirm']) || empty($_POST['password-confirm'])) ||
+			(!isset($_POST['country']) || empty($_POST['country'])) ||
+			(!isset($_POST['description']) || empty($_POST['description'])) ||
+			(!isset($_POST['website']) || empty($_POST['website']))){
 				App::error('Vous devez remplir tous les champs obligatoires');
 			}
 
-			if(!preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name'])){
+			if (!preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name'])){
 				App::error("Veuillez entrer un nom approprié");
 			}
 
-			if(!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email'])){
+			if (!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email'])){
 				App::error("Veuillez entrer un email approprié");
 			}
 
-			if(!preg_match("#^[a-zA-Z\@._-]{2,32}#", $_POST['password'])){
+			if (!preg_match("#^[a-zA-Z\@._-]{2,32}#", $_POST['password'])){
 				App::error("Veuillez entrer un mot de passe approprié");
 			}
-			if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['website'])){
+
+			if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['website'])){
 				App::error("Veuillez entrer une adresse web appropriée");
 			}
-			if($_POST['email']!=$_POST['email-confirm']){
+
+			if ($_POST['email']!=$_POST['email-confirm']){
 				App::error("L'adresse email doit correspondre");
 			}
-			if($_POST['password']!=$_POST['password-confirm']){
+
+			if ($_POST['password']!=$_POST['password-confirm']){
 				App::error("Le mot de passe doit correspondre");
 			}
-			if(!isset($_POST['accept_terms'])){
+
+			if (!isset($_POST['accept_terms'])){
 				App::error("Vous devez accepter les conditions d'utilisation");
+			}
+
+			if (!isset($_POST['g-recaptcha-response'])){
+				App::error("Vous devez confirmer que vous n'êtes pas un robot");
 			}
 		}
 	}
@@ -182,11 +197,20 @@
 					</div>
 				</div>
 
+				<div class="row">
+					<div class="col-md-12">
+						<label for="signup-terms">Code de vérification</label>
+						<div class="g-recaptcha" data-sitekey="6Lc2cBoTAAAAAF9TTSA9CRMbg-jss8X-CmDy_eXI"></div>
+						<p class="help-block">Vérifiez que vous êtes un humain</p>
+					</div>
+				</div>
+
 				<input type="submit" name="register" class="btn btn-primary" value="S'inscrire">
 			</form>
-            
-            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		</div>
 	</div>
 </div>
-</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/jquery.form-validator.min.js"></script>
+<script src="https://www.google.com/recaptcha/api.js"></script>
