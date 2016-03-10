bca81f75b66580939879ce5429adb33273bc79f3
ValGou <val.gougeon@hotmail.fr>
2016-03-10 21:26:41 +0100

    Upload CV + modif password

--- PHP/app/view/profile-student.php
+++ PHP/app/view/profile-student.php
@@ -3,36 +3,64 @@
 		$student = Student::getStudentById($_SESSION['id']);
 
 		if (isset($_POST['edit'])) :
-			if ($_POST['password'] == $_POST['password-confirm']) {
+			if (isset($_POST['password']) && $_POST['password'] == $_POST['password-confirm']) {
 				if (Bcrypt::checkPassword($_POST['password'], $student->password)) {
-					PDOConnexion::setParameters('phonedeals', 'root', 'root');
-					$db = PDOConnexion::getInstance();
-					$sql = "
-						UPDATE student
-						SET cv = :cv,
-							portfolio = :portfolio
-						WHERE id = :id
-					";
-					$sth = $db->prepare($sql);
-					$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Student');
-					$sth->execute(array(
-						':id' => $student->id,
-						':cv' => $_POST['cv'],
-						':portfolio' => $_POST['portfolio']
-					));
+					if(isset($_POST['new-password']) && $_POST['new-password']!=''){
+						if(preg_match("#^[a-zA-Z\@._-]{2,32}#", $_POST['new-password'])){
+							$new_password = Bcrypt::hashPassword($_POST['new-password']);
+							Student::changePassword($new_password, $student->id);							
+							App::success('Votre mot de passe a bien été modifié');
+						}
+						else{
+							App::error('Veuillez entrer un nouveau mot de passe approprié');
+						}	
+					}
 
-					if ($sth) {
-						App::success('Vos informations ont bien été modifiées');
+					if(isset($_POST['portfolio']) && $_POST['portfolio']!='' && $_POST['portfolio']!=$student->portfolio){
+						if(preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['portfolio'])){
+							Student::changePortfolio($_POST['portfolio'], $student->id);							
+							App::success('Votre portfolio a bien été modifié');
+						}
+						else{
+							App::error('Veuillez entrer un url approprié');
+						}	
+					}
+
+					if(isset($_FILES['cv']) && $_FILES['cv']['name']!=''){
+
+						$my_file = basename($_FILES['cv']['name']);
+						$max_file_size = 6000000;
+						$file_size = filesize($_FILES['cv']['tmp_name']);
+						$file_ext = strrchr($_FILES['cv']['name'], '.'); 
+
+						if($file_ext == '.pdf' && $file_size < $max_file_size){	
+							$folder = "uploads/cv";
+          					$file = $folder . '/' . $student->id . '.pdf';
+          					if(file_exists($file)){
+          						unlink($file);
+          					}
+         					move_uploaded_file($_FILES['cv']['tmp_name'], $file);
+
+							App::success('Votre CV a bien été modifié');
+						}
+						else{
+							if($file_ext != '.pdf'){							
+								App::error('Votre CV doit être au format PDF');
+							}
+							if($file_size > $max_file_size){							
+								App::error('Votre CV est trop lourd, choisissez un autre fichier');
+							}
+						}	
 					}
 				}
 
 				else {
-					echo App::error('Le mot de passe entré est incorrect, veuillez réessayer.');
+					echo App::error('Le mot de passe entré est incorrect, veuillez réessayer');
 				}
 			}
 
 			else {
-				echo App::error('Les deux mots de passes ne correspondent pas');
+				echo App::error('Les deux mots de passe ne correspondent pas');
 			}
 		else:
 ?>
@@ -46,7 +74,7 @@
 				</div>
 				<div class="row">
 					<div class="col-md-9">
-						<form action="index.php?page=profile" method="POST" class="form-horizontal">
+						<form action="index.php?page=profile-student" method="POST" class="form-horizontal" enctype="multipart/form-data">
 							<div class="form-group">
 								<label class="col-sm-2 control-label">Nom</label>
 								<div class="col-sm-10">
@@ -91,6 +119,13 @@
 							</div>
 
 							<div class="form-group">
+								<label for="profile-new-password" class="col-sm-2 control-label">Nouveau mot de passe</label>
+								<div class="col-sm-10">
+									<input type="password" class="form-control" id="profile-new-password" name="new-password" placeholder="Nouveau mot de passe">
+								</div>
+							</div>
+
+							<div class="form-group">
 								<label for="profile-portfolio" class="col-sm-2 control-label">Portfolio</label>
 								<div class="col-sm-10">
 									<input type="url" class="form-control" id="profile-portfolio" name="portfolio" value="<?php echo $student->portfolio; ?>" placeholder="Lien du portfolio">
@@ -98,16 +133,16 @@
 							</div>
 
 							<div class="form-group">
-								<label for="profile-password" class="col-sm-2 control-label">Mot de passe</label>
+								<label for="profile-password" class="col-sm-2 control-label">Entrer votre mot de passe</label>
 								<div class="col-sm-10">
-									<input type="password" class="form-control" id="profile-password" name="password" placeholder="Mot de passe">
+									<input type="password" class="form-control" id="profile-password" required='required' name="password" placeholder="Mot de passe actuel">
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="profile-password-confirm" class="col-sm-2 control-label">Confirmer</label>
+								<label for="profile-password-confirm" class="col-sm-2 control-label">Confirmer votre mot de passe</label>
 								<div class="col-sm-10">
-									<input type="password" class="form-control" id="profile-password-confirm" name="password-confirm" placeholder="Confirmer le mot de passe">
+									<input type="password" class="form-control" id="profile-password-confirm" required='required' name="password-confirm" placeholder="Confirmer le mot de passe">
 									<p class="help-block">Entrez votre mot de passe pour confirmer votre identité et valider les modifications</p>
 								</div>
 							</div>
