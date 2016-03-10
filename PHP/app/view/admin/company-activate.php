0ec4a3a8dde484926f87ed056b603e6c39f89690
Hadrien <hadriien@live.fr>
2016-03-10 17:14:51 +0100

    Added files via upload
    Ajout de la page permettant de valider une entreprise

---
+++ PHP/app/view/admin/company-activate.php
@@ -0,0 +1,24 @@
+<?php
+	if (isset($_GET['id']) && !empty($_GET['id'])) {
+		$id = htmlentities($_GET['id']);
+		$company = Company::getCompanyById($id);
+		
+		if (!$company->activated) {
+			if (Company::activateCompany($id)) {
+				App::redirect('index.php?page=admin/companies-list');
+			}
+
+			else {
+				App::error('Erreur lors de la validation de l\'entreprise');
+			}
+		}
+
+		else {
+			App::error('Cette entreprise a déjà été confirmée');
+		}
+	}
+
+	else {
+		App::redirect('index.php?page=admin/students-list');
+	}
+?>
