934c1bcdce211c1ab68ec00ef6b471023efac0b6
Hadrien <hadriien@live.fr>
2016-03-10 17:08:50 +0100

    Added files via upload
    Ajout de la page pour valider un étudiant

---
+++ PHP/app/view/admin/student-activate.php
@@ -0,0 +1,24 @@
+<?php
+	if (isset($_GET['id']) && !empty($_GET['id'])) {
+		$id = htmlentities($_GET['id']);
+		$student = Student::getStudentById($id);
+		
+		if (!$student->activated) {
+			if (Student::activateStudent($id)) {
+				App::redirect('index.php?page=admin/students-list');
+			}
+
+			else {
+				App::error('Erreur lors de la validation de l\'étudiant');
+			}
+		}
+
+		else {
+			App::error('Cet étudiant a déjà été confirmé');
+		}
+	}
+
+	else {
+		App::redirect('index.php?page=admin/students-list');
+	}
+?>
