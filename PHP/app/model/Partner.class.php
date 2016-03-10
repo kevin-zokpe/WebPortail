2cbcf4e5779575a96d6d8713edeace2e56d6bee7
ValGou <val.gougeon@hotmail.fr>
2016-03-11 00:41:51 +0100

    modele Partner

---
+++ PHP/app/model/Partner.class.php
@@ -0,0 +1,50 @@
+<?php	
+	class Partner {
+		private $id;
+		private $name;
+		private $logo;
+		private $country;
+		private $register_date;
+		
+		public function __construct(array $args = array()) {
+			if (!empty($args)) {
+				foreach($args as $p => $v) {
+					$this->$p = $v;
+				}
+			}
+		}
+
+		public function __get($nom){
+			return $this->$nom;
+		}
+
+		public function __set($n, $v){
+			$this->$n = $v;
+		}
+
+		public static function getPartnerById($id) {
+			PDOConnexion::setParameters('stages', 'root', 'root');
+			$db = PDOConnexion::getInstance();
+			$sql = 'SELECT * FROM partner WHERE id = :id';
+			$sth = $db->prepare($sql);
+			$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Partner');
+			$sth->execute(array(
+				':id' => $id
+			));
+			
+			return $sth->fetch();
+		}
+
+		public static function getPartnersList() {
+			PDOConnexion::setParameters('stages', 'root', 'root');
+			$db = PDOConnexion::getInstance();
+			$sql = 'SELECT * FROM partner';
+			$sth = $db->prepare($sql);
+			$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Partner');
+			$sth->execute();
+			
+			return $sth->fetchAll();
+		}
+
+	}
+?>
