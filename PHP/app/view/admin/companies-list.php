c673a8ababf859005e752c9192081d1bc3927169
Hadrien <hadriien@live.fr>
2016-03-10 10:50:02 +0100

    Update companies-list.php
    Ajout du tri par activé/non activé

--- PHP/app/view/admin/companies-list.php
+++ PHP/app/view/admin/companies-list.php
@@ -1,8 +1,32 @@
+<?php
+	if (!isset($_GET['type']) || $_GET['type'] != 'awaiting') {
+		$_GET['type'] = 'activated';
+	}
+
+	$type = htmlentities($_GET['type']);
+?>
 <div class="col-md-12">
 	<div class="page-header">
-		<h1>Liste des entreprises</h1>
+		<h1>
+			Liste des entreprises
+			<?php
+				if ($type == 'awaiting') {
+					echo '<small>En attente d\'activation</small>';
+					echo '<a href="index.php?page=admin/companies-list&amp;type=activated" class="btn btn-primary pull-right">Voir les entreprises confirmées <span class="badge">' . count(Company::getActivatedCompanies(true)) . '</span></a>';
+				}
+
+				else {
+					echo '<small>Confirmées</small>';
+					echo '<a href="index.php?page=admin/companies-list&amp;type=awaiting" class="btn btn-primary pull-right">En attente de confirmation <span class="badge">' . count(Company::getActivatedCompanies(false)) . '</span></a>';
+				}
+			?>
+		</h1>
 	</div>
 	
+	<?php
+		if ($type == 'activated') :
+			if (Company::getActivatedCompanies(true)) :
+	?>
 	<table class="table table-striped">
 		<thead>
 			<tr>
@@ -18,7 +42,7 @@
 			</tr>
 		</thead>
 		<?php
-			foreach (Company::getCompaniesList() as $company) {
+			foreach (Company::getActivatedCompanies() as $company) {
 				echo '<tr>';
 					echo '<td>' . $company->id . '</td>';
 					echo '<td>' . $company->name . '</td>';
@@ -27,10 +51,54 @@
 					echo '<td>' . $company->city . '</td>';
 					echo '<td>' . $company->description . '</td>';
 					echo '<td>' . $company->website . '</td>';
-					echo '<td><a href="index.php?page=admin/company-edit&amp;id=' . $company->id . '"><i class="fa fa-pencil" data-toggle="tooltip" title="Modifier"></i></a></td>';
-					echo '<td><a href="#"><i class="fa fa-trash" data-toggle="tooltip" title="Supprimer"></i></a></td>';
+					echo '<td><a href="index.php?page=admin/company-edit&amp;id=' . $company->id . '"><i class="fa fa-pencil"></i></a></td>';
+					echo '<td><a href="#"><i class="fa fa-trash"></i></a></td>';
 				echo '</tr>';
 			}
 		?>
 	</table>
+	<?php
+			else:
+				echo 'Aucune entreprise n\'a été confirmée.';
+			endif;
+		else:
+			if (Company::getActivatedCompanies(false)) :
+	?>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nom</th>
+					<th>Prénom</th>
+					<th>Pays</th>
+					<th>Compétence</th>
+					<th>Email</th>
+					<th>Portfolio</th>
+					<th>Date d'inscription</th>
+					<th></th>
+					<th></th>
+				</tr>
+			</thead>
+			<?php
+				foreach (Company::getActivatedCompanies(false) as $company) {
+					echo '<tr>';
+						echo '<td>' . $company->id . '</td>';
+						echo '<td>' . $company->name . '</td>';
+						echo '<td>' . $company->email . '</td>';
+						echo '<td>' . $company->country . '</td>';
+						echo '<td>' . $company->city . '</td>';
+						echo '<td>' . $company->description . '</td>';
+						echo '<td>' . $company->website . '</td>';
+						echo '<td><a href="index.php?page=admin/company-edit&amp;id=' . $company->id . '"><i class="fa fa-pencil"></i></a></td>';
+						echo '<td><a href="#"><i class="fa fa-trash"></i></a></td>';
+					echo '</tr>';
+				}
+			?>
+		</table>
+	<?php
+			else:
+				echo 'Aucune entreprise n\'est en attente de confirmation.';
+			endif;
+		endif;
+	?>
 </div>
