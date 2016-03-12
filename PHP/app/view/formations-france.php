bb452fdd41f7002ee275015bbf6b9518dc9cd6ac
ValGou <val.gougeon@hotmail.fr>
2016-03-12 15:01:51 +0100

    Update formations-france.php

--- PHP/app/view/formations-france.php
+++ PHP/app/view/formations-france.php
@@ -1,25 +1,64 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<h2>Formations en France</h2>
-			<h3>Métiers du Multimédia et de l'Internet</h3>
+			<h1>Formations <small>en France</small></h1>
+      <div class="col-md-offset-1 col-md-8"> Les étudiants Irlandais que vous trouverez sur Webportal sont ici de prestigieuses universités Irlandaises. <br/>Ici sont détaillés l'ensemble des cours dispensés à vos futurs stagiaires Irlandais !</div><br /><br />
+			<h2> IUT Cherbourg Manche - <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/" target="_blank"> Diplômes Universitaires de Technologie</a></h2>
 			<br />
-			<div class="row">
-				<div class="col-md-offset-2 col-md-8">
-					<p>Le DUT Métiers du Multimédia et de l'Internet (MMI) est une formation professionnalisante en deux ans. 
-						Divisé en quatre semestres, ce DUT forme les étudiants à devenir des techniciens polyvalents dans les domaines du web, du réseau, 
-						de l'audiovisuel et de la communication. Le DUT s'achève par un stage de 10 semaines minimum. Ce DUT fourni une connaissance des 
-						métiers de la communication associée à une maîtrise des outils de production et de développement en informatique et multimédia. </p>
-				</div>
-			</div>
+			
+			 <div class="col-xs-6 col-md-4">
+    			<a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/mmi-multimedia-et-internet-/" target="_blank" class="thumbnail">
+      				<img src="public/img/formations-irlande/business.jpg" alt="MMI">
+      				<div class="caption">
+        				<h3>Métiers du Mutlimdéia et de l'Internet</h3>
+        			</div>
+			    </a>
+			  </div>
 
-			<h3>Génie Thermique et Énergie</h3>
-			<br />
-			<div class="row">
-				<div class="col-md-offset-2 col-md-8">
-					<p></p>
-				</div>
-			</div>
+			  <div class="col-xs-6 col-md-4">
+    			<a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/gte-energie-environnement-/" target="_blank" class="thumbnail">
+      				<img src="public/img/formations-irlande/humanities.jpg" alt="GTE">
+      				<div class="caption">
+        				<h3>Génie Thermique et Énergie</h3>
+        			</div>
+			    </a>
+			  </div>
+
+ 			  <div class="col-xs-6 col-md-4">
+    			<a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/gea-gestion-/" target="_blank" class="thumbnail">
+      				<img src="public/img/formations-irlande/science.jpg" alt="GEA">
+      				<div class="caption">
+        				<h3>Gestion des Entreprises et des Administrations</h3>
+        			</div>
+			    </a>
+			  </div>
+
+			  <div class="col-xs-6 col-md-4">
+    			<a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/tc-commerce-/" target="_blank" class="thumbnail">
+      				<img src="public/img/formations-irlande/sport.jpg" alt="TC">
+      				<div class="caption">
+        				<h3>Techniques de Commercialisation</h3> 
+        			</div>
+			    </a>
+			  </div>
+
+			  <div class="col-xs-6 col-md-4">
+    			<a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/gim-ingenierie-developpement-durable-/" target="_blank" class="thumbnail">
+      				<img src="public/img/formations-irlande/game.jpg" alt="GIM">
+      				<div class="caption">
+        				<h3>Génie Industriel et Maintenance</h3>
+        			</div>
+			    </a>
+			  </div>
+
+			  <div class="col-xs-6 col-md-4">
+    			<a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/geii-reseaux-automatismes-nouvelles-technologies-/" target="_blank" class="thumbnail">
+      				<img src="public/img/formations-irlande/game.jpg" alt="GEII">
+      				<div class="caption">
+        				<h3>Génie Electrique et Informatique Industrielle</h3>
+        			</div>
+			    </a>
+			  </div>
 		</div>
 	</div>
 </div>
