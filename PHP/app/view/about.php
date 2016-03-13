f7e8acb7d74580cf73b65248636ca8f022248398
Hadrien <hadriien@live.fr>
2016-03-13 14:36:41 +0100

    Added files via upload
    Ajout de la page "à propos"

---
+++ PHP/app/view/about.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1>À propos</h1>
+			</div>
+			<h1>CONDITIONS D'UTILISATION DU SITE</h1>
+			<h3>1. A PROPOS DE NOUS</h3>
+			<p> Un groupe de cinq étudiants à l’IUT MMI Cherbourg-Manche, antenne de Saint-Lô composé de Ange-Kevin Zokpe, Sonia Hamelin, Hadrien Rannou, Gougeon Valentin et Mikhail Orlov-Sytenkov. Nous avons tous travaillé sur le projet, que ce soit pour le côté graphique comme le logo, ou le codage du site, tels que les formulaires et d'autres interactions. </p>
+			<h3>2. A PROPOS DU CLIENT</h3>
+			<p> Ce portail de stages nous a été demandé par Jérôme Clouët, professeur d’anglais à l’IUT MMI. Il nous a défini un cahier de charges, à partir duquel nous avons dû créer ce site internet. Cette demande a été faite dans le cadre des projets tuteurés de l’IUT. Il est également notre traducteur français – anglais. </p>
+			<h3>3. A PROPOS DU PORTAIL</h3>
+			<p> Ceci est un portail de stages liant l’IUT MMI Cherbourg-Manche, antenne de Saint-Lô à LYIT, à Letterkenny (Irlande).  Entreprises et étudiants peuvent se connecter ici pour respectivement proposer et chercher des stages dans les pays partenaires. </p>
+			<h3>4. QUE PROPOSONS NOUS</h3>
+			<p> La possibilité de mettre des offres de stage sur le site si vous êtes représentant d’entreprise, ou de trouver des offres de stage si vous êtes étudiant. Ce seront des stages avec plusieurs compétences requises possibles, qui seront définis dans la demande. Vous pouvez vous renseigner sur nos entreprises partenaires en vous référant aux  partenaires cités sur la page d’accueil.</p>
+		</div>
+	</div>
+</div>
