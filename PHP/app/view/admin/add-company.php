14382c098a901a8d706ee9fe7cfcb06a3748cbcd
ValGou <val.gougeon@hotmail.fr>
2016-03-16 12:50:18 +0100

    Update add-company.php

--- PHP/app/view/admin/add-company.php
+++ PHP/app/view/admin/add-company.php
@@ -3,6 +3,13 @@
 		$id = $_SESSION['id'];
 		$admin_password = Student::getStudentById($id)->password;
 
+		if(isset($_FILES['logo'])){
+			$my_file = basename($_FILES['logo']['name']);
+			$max_file_size = 6000000;
+			$file_size = filesize($_FILES['logo']['tmp_name']);
+			$file_ext = strrchr($_FILES['logo']['name'], '.'); 
+		}
+
 		if (isset($_POST['add-company'])) {
 			if (isset($_POST['name']) && !empty($_POST['name'])) {
 				if (isset($_POST['email']) && !empty($_POST['email']) && Company::checkEmailExist($_POST['email'])==false) {
@@ -10,7 +17,7 @@
 						if (isset($_POST['city']) && !empty($_POST['city'])) {
 							if (isset($_POST['description']) && !empty($_POST['description'])) {
 								if (isset($_POST['website']) && !empty($_POST['website'])) {
-
+				
 									PDOConnexion::setParameters('stages', 'root', 'root');
 									$db = PDOConnexion::getInstance();
 									$sql = "
@@ -28,9 +35,40 @@
 										':password' => $admin_password,
 										':website' => $_POST['website']
 									));
+
+									if (isset($_FILES['logo']) && ($file_ext == '.jpg' || $file_ext == '.png') && $file_size < $max_file_size){
+
+										$id_company = Company::getCompanyIDByEmail($_POST['email']);
+
+										$folder = "uploads/companies";
+										if($file_ext == '.jpg')
+          									$file = $folder . '/' . $id_company->id . '.jpg';
+          								if($file_ext == '.png')
+          									$file = $folder . '/' . $id_company->id . '.png';
+         								move_uploaded_file($_FILES['logo']['tmp_name'], $file);
+
+         								PDOConnexion::setParameters('stages', 'root', 'root');
+										$dbh = PDOConnexion::getInstance();
+										$req = "UPDATE company SET logo = :logo WHERE id = :id";
+										$st = $dbh->prepare($req);
+										$st->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Company');
+										$st->execute(array(
+											':logo' => $file,
+											':id' => $id_company->id
+										));
+
+									}
+									else {
+										if ($file_ext != '.jpg' && $file_ext != '.png'){							
+											$msg->error('Le logo doit être au format JPG ou PNG','index.php?page=admin/add-company');
+										}
+										if ($file_size > $max_file_size){							
+											$msg->error('Le logo est trop lourd, choisissez un autre fichier','index.php?page=admin/add-company');
+										}
+									}
 									
 									if ($sth) {
-										$msg->success('L\'entreprise à bien été ajoutée. <br /> Son mot de passe est le vôtre. <br /> Vous pourez le changer ultérieurement');
+										$msg->success('L\'entreprise à bien été ajoutée. <br /> Son mot de passe est le vôtre. <br /> Vous pourez le changer ultérieurement', 'index.php?page=admin/companies-list');
 									}
 								
 								else {
@@ -74,7 +112,7 @@
 				<div class="page-header">
 					<h1> Ajouter une entreprise </h1>
 				</div>
-					<form action="index.php?page=admin/add-company" method="POST">
+					<form action="index.php?page=admin/add-company" method="POST" enctype="multipart/form-data">
 
 							<div class="row">
 								<div class="col-md-12">
@@ -106,6 +144,13 @@
 							</div>
 
 							<div class="row">
+								<div class="col-md-6">
+									<label for="signup-logo">Logo</label>
+									<input type="file" name="logo" id="signup-logo" placeholder="Insérer votre logo" required="required" data-validation-error-msg="Vous devez insérer un logo !">
+								</div>
+							</div>
+
+							<div class="row">
 								<div class="col-md-12">
 									<label for="signup-description">Description</label>
 									<textarea name="description" id="signup-description" class="form-control" rows="3" placeholder="Description" data-validation-length="2-30"  data-validation-error-msg="Rédigez une description de l'entreprise !" required></textarea>
@@ -118,7 +163,7 @@
 									<input type="text" name="website" class="form-control" id="signup-website" placeholder="Site web" data-validation="length" data-validation-length="16-128" data-validation-error-msg="Vous devez entrer un site web !">
 								</div>
 							</div>
-							<button type="submit" class="btn btn-primary" name="add-company">Ajouter le stage</button>
+							<button type="submit" class="btn btn-primary" name="add-company">Ajouter</button>
 						</form>
 			</div>
 		</div>
