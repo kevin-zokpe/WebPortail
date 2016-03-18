3f34374915250555c41048e0e2fe26c306e7191e
Hadrien <hadriien@live.fr>
2016-03-18 18:06:05 +0100

    Update student-edit.php
    Code amélioré et ajout des méthodes editStudent et editCv

--- PHP/app/view/admin/student-edit.php
+++ PHP/app/view/admin/student-edit.php
@@ -9,7 +9,7 @@
 			$my_file = basename($_FILES['cv']['name']);
 			$max_file_size = 6000000;
 			$file_size = filesize($_FILES['cv']['tmp_name']);
-			$file_ext = strrchr($_FILES['cv']['name'], '.'); 
+			$file_ext = strrchr($_FILES['cv']['name'], '.');
 
 			if (isset($_POST['first_name']) && !empty($_POST['first_name']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['first_name']) &&
     		isset($_POST['last_name']) && !empty($_POST['last_name']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['last_name']) &&
@@ -17,83 +17,48 @@
     	   	isset($_POST['country'] ) && !empty($_POST['country']) &&
     	   	isset($_POST['skill']) && !empty($_POST['skill']) &&
     	   	preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['portfolio'])) {
+    	   		$editInformations = Student::editStudent($id, $_POST['first_name'], $_POST['last_name'], $_POST['country'], $_POST['skill'], $_POST['email'], $_POST['portfolio'], $_POST['activated']);
 
+				if (isset($_FILES['cv']) && !empty($_FILES['cv']['name'])) {
+					$my_file = basename($_FILES['cv']['name']);
+					$max_file_size = 6000000;
+					$file_size = filesize($_FILES['cv']['tmp_name']);
+					$file_ext = strrchr($_FILES['cv']['name'], '.');
 
+					if ($file_ext == '.pdf' && $file_size < $max_file_size) {
+						$folder = 'uploads/cv';
+      					$file = $folder . '/' . $id . '.pdf';
+      					
+      					if (file_exists($file)) {
+      						unlink($file);
+      					}
 
-				PDOConnexion::setParameters('stages', 'root', 'root');
-				$db = PDOConnexion::getInstance();
-				$sql = "
-					UPDATE student
-					SET first_name = :first_name,
-						last_name = :last_name,
-						country = :country,
-						skill = :skill,
-						email = :email,
-						portfolio = :portfolio,
-						activated = :activated
-					WHERE id = :id
-				";
-				$sth = $db->prepare($sql);
-				$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Student');
-				$sth->execute(array(
-					':id' => $id,
-					':first_name' => $_POST['first_name'],
-					':last_name' => $_POST['last_name'],
-					':country' => $_POST['country'],
-					':skill' => $_POST['skill'],
-					':email' => $_POST['email'],
-					':portfolio' => $_POST['portfolio'],
-					':activated' => $_POST['activated']
-				));
+     					move_uploaded_file($_FILES['cv']['tmp_name'], $file);
 
-				if (isset($_FILES['cv']) && $_FILES['cv']['name']!=''){
+     					$editCv = Student::editCv($id, $file);
 
-						$my_file = basename($_FILES['cv']['name']);
-						$max_file_size = 6000000;
-						$file_size = filesize($_FILES['cv']['tmp_name']);
-						$file_ext = strrchr($_FILES['cv']['name'], '.'); 
+						if ($editCv) {
+     						$msg->success('Le CV de l\'étudiant a bien été modifié.', 'index.php?page=admin/students-list');
+     					}
+					}
 
-						if ($file_ext == '.pdf' && $file_size < $max_file_size){
-
-							$folder = "uploads/cv";
-          					$file = $folder . '/' . $id . '.pdf';
-          					if(file_exists($file)) unlink($file);
-         					move_uploaded_file($_FILES['cv']['tmp_name'], $file);
-
-         					PDOConnexion::setParameters('stage', 'root', 'root');
-							$dbh = PDOConnexion::getInstance();
-							$req = "
-								UPDATE student
-								SET cv = :cv
-								WHERE id = :id
-							";
-							$stt = $dbh->prepare($req);
-							$stt->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Student');
-							$stt->execute(array(
-								':id' => $id,
-								':cv' => $file
-							));
-
-							if ($stt) {
-         						$msg->success("Le CV de l'étudiant a bien été modifié.",'index.php?page=admin/students-list');
-         					}
+					else {
+						if ($file_ext != '.pdf') {							
+							$msg->error('Le CV doit être au format PDF','index.php?page=admin/student-edit&id=' . $id);
 						}
-						else {
-							if ($file_ext != '.pdf'){							
-								$msg->error('Le CV doit être au format PDF','index.php?page=admin/student-edit&id=' . $id);
-							}
-							if ($file_size > $max_file_size){							
-								$msg->error('Le CV est trop lourd, choisissez un autre fichier','index.php?page=admin/student-edit&id=' . $id);
-							}
-						}	
+
+						if ($file_size > $max_file_size) {							
+							$msg->error('Le CV est trop lourd, choisissez un autre fichier','index.php?page=admin/student-edit&id=' . $id);
+						}
+					}
 			
-					if ($sth) {
-						$msg->success('Cet étudiant a bien été modifié.','index.php?page=admin/students-list');
+					if ($editInformations) {
+						$msg->success('Cet étudiant a bien été modifié.', 'index.php?page=admin/students-list');
 					}
 				}
 			}
-			else{
 
+			else {
 				if ((!isset($_POST['first_name']) || empty($_POST['first_name'])) || 
 				(!isset($_POST['last_name']) || empty($_POST['last_name'])) ||
 				(!isset($_POST['email']) || empty($_POST['email'])) ||
@@ -117,7 +82,6 @@
 				if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['portfolio'])){
 					$msg->error("Veuillez entrer une adresse web appropriée",'index.php?page=admin/student-edit&id=' . $id);
 				}
-
 			}
 		}
 
