9a911af59295ba64ea58648fe03510a6d24e114a
ValGou <val.gougeon@hotmail.fr>
2016-03-18 19:49:35 +0100

    Update find-internship.php

--- PHP/app/view/find-internship.php
+++ PHP/app/view/find-internship.php
@@ -11,11 +11,11 @@
 						}
 
 						else {
-							var table = '<table class="table table-striped"><thead><tr><th>Entrepise</th><th>Poste</th><th>Mission</th><th>Adresse</th><th>Ville</th><th>Code postal</th><th></th></tr></thead><tbody>';
+							var table = '<table class="table table-striped"><thead><tr><th>Entrepise</th><th>Poste</th><th>Mission</th><th>Adresse</th><th>Ville</th><th>Code postal</th><th></th><th></th></tr></thead><tbody>';
 							
 							for (i = 0; i < result.length; i++) {
 								var internshipInfo = result[i];
-								table += '<tr><td><a href="index.php?page=view-profile-company&id=' + internshipInfo.id_company + '">' + internshipInfo.company + '</a></td><td>' + internshipInfo.name + '</td><td>' + internshipInfo.description + '</td><td>' + internshipInfo.address + '</td><td>' + internshipInfo.city + '</td><td>' + internshipInfo.zip_code + '</td>' + '<td><i class="fa fa-envelope"></i> <a href="mailto:' + internshipInfo.email + '">Contacter</a></td>';
+								table += '<tr><td><a href="index.php?page=view-profile-company&id=' + internshipInfo.id_company + '">' + internshipInfo.company + '</a></td><td>' + internshipInfo.name + '</td><td>' + internshipInfo.description + '</td><td>' + internshipInfo.address + '</td><td>' + internshipInfo.city + '</td><td>' + internshipInfo.zip_code + '</td>' + '<td><i class="fa fa-envelope"></i> <a href="mailto:' + internshipInfo.email + '">Contacter</a></td>' + '<td><i class="fa fa-file-text"></i> <a href="index.php?page=internship-info&id=' + internshipInfo.id + '">Voir la fiche</a></td>';
 							}
 
 							table += '</tbody></table>';
