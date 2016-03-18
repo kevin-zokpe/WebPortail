9078c195b0e6a0c84c1bfe6eb0b0da4d6d8ff011
Hadrien <hadriien@live.fr>
2016-03-19 00:10:36 +0100

    Ajout de vérifications
    - Vérification du pays, s'il est différent de nos attentes, on le définit comme étant la France
    - On affecte une variable pour le nom, puisque France en Anglais c'est pareil, par contre Irlande c'est Ireland, et justement on ne veut que des URL écrites en Anglais
    - Correction des redirections pour ne pas oublier de préciser le pays, sinon on aurait une erreur, soit une autre redirection et donc aucun message de succès ou d'erreur

--- PHP/app/view/admin/add-internship.php
+++ PHP/app/view/admin/add-internship.php
@@ -1,6 +1,12 @@
 <?php
 	if (App::isAdmin()) :
-		$country = $_GET['country'];
+		$country = htmlentities($_GET['country']);
+
+		if ($country != 'france' || $country != 'irelande') {
+			$country = 'france';
+		}
+
+		$countryName = ($country == 'irelande') ? 'Irlande' : 'France';
 
 		if (isset($_POST['add-internship'])) {
 			if (isset($_POST['name']) && !empty($_POST['name'])) {
@@ -10,69 +16,71 @@
 							if (isset($_POST['zip_code']) && !empty($_POST['zip_code'])) {
 								if (isset($_POST['city']) && !empty($_POST['city'])) {
 									if (isset($_POST['skill']) && !empty($_POST['skill'])) {
-									PDOConnexion::setParameters('stages', 'root', 'root');
-									$db = PDOConnexion::getInstance();
-									$sql = "
-										INSERT INTO internship (name, description, company, address, zip_code, city, skill)
-										VALUES (:name, :description, :company, :address, :zip_code, :city, :skill)
-									";
-									$sth = $db->prepare($sql);
-									$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Internship');
-									$sth->execute(array(
-										':name' => $_POST['name'],
-										':description' => $_POST['description'],
-										':company' => $_POST['company'],
-										':address' => $_POST['address'],
-										':zip_code' => $_POST['zip_code'],
-										':city' => $_POST['city'],
-										':skill' => $_POST['skill']
-									));
-									
-									if ($sth) {
-										$msg->success('Le stage à bien été ajouté.','index.php?page=admin/internship-list');
+										PDOConnexion::setParameters('stages', 'root', 'root');
+										$db = PDOConnexion::getInstance();
+										$sql = "
+											INSERT INTO internship (name, description, company, address, zip_code, city, skill)
+											VALUES (:name, :description, :company, :address, :zip_code, :city, :skill)
+										";
+										$sth = $db->prepare($sql);
+										$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Internship');
+										$sth->execute(array(
+											':name' => $_POST['name'],
+											':description' => $_POST['description'],
+											':company' => $_POST['company'],
+											':address' => $_POST['address'],
+											':zip_code' => $_POST['zip_code'],
+											':city' => $_POST['city'],
+											':skill' => $_POST['skill']
+										));
+										
+										if ($sth) {
+											$msg->success('Le stage à bien été ajouté.', 'index.php?page=admin/internship-list&country=' . $country);
+										}
+									}
+
+									else {
+										$msg->error('Vous devez renseigner le domaine d\'activité du stage.', 'index.php?page=admin/add-internship&country=' . $country);
 									}
 								}
 
 								else {
-									$msg->error('Vous devez renseigner le domaine d\'activité du stage.','index.php?page=admin/add-internship');
+									$msg->error('Vous devez renseigner la ville où se déroule le stage.', 'index.php?page=admin/add-internship&country=' . $country);
 								}
 							}
 
 							else {
-								$msg->error('Vous devez renseigner la ville où se déroule le stage.','index.php?page=admin/add-internship');
+								$msg->error('Vous devez renseigner le code postal du lieu où se déroule le stage.', 'index.php?page=admin/add-internship&country=' . $country);
 							}
 						}
 
 						else {
-							$msg->error('Vous devez renseigner le code postal du lieu où se déroule le stage.','index.php?page=admin/add-internship');
+							$msg->error('Vous devez renseigner l\'adresse où se déroule le stage.', 'index.php?page=admin/add-internship&country=' . $country);
 						}
 					}
 
 					else {
-						$msg->error('Vous devez renseigner l\'adresse où se déroule le stage.','index.php?page=admin/add-internship');
+						$msg->error('Vous devez renseigner la description du stage.', 'index.php?page=admin/add-internship&country=' . $country);
 					}
 				}
 
 				else {
-					$msg->error('Vous devez renseigner la description du stage.','index.php?page=admin/add-internship');
+					$msg->error('Vous devez renseigner le poste du stage.', 'index.php?page=admin/add-internship&country=' . $country);
 				}
 			}
+
 			else {
-				$msg->error('Vous devez renseigner le poste du stage.','index.php?page=admin/add-internship');
+				$msg->error('Vous devez renseigner l\'entreprise.', 'index.php?page=admin/add-internship&country=' . $country);
 			}
 		}
-		else {
-			$msg->error('Vous devez renseigner l\'entreprise.','index.php?page=admin/add-internship');	
-		}
-	}
 
-		else {
-	?>
+	else {
+?>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="page-header">
-					<h1> Ajouter un stage en <?php echo $country;?> </h1>
+					<h1>Ajouter un stage en <?php echo $countryName;?></h1>
 				</div>
 				<form action="index.php?page=admin/add-internship&amp;country=<?php echo $country; ?>" method="POST">
 					<div class="row" style="margin-bottom: 20px;">
@@ -163,6 +171,6 @@
 	}
 
 	else:
-		App::redirect('index.php?page=admin/internships-list&country=France');
+		App::redirect('index.php?page=admin/internships-list&country=' . $country);
 	endif;
 ?>
