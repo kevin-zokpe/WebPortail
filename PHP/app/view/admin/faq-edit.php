ce316380245757074408cd68340bda74e336b14f
Hadrien <hadriien@live.fr>
2016-03-18 18:30:18 +0100

    Amélioration de la sécurité et correction d'un bug
    - On vérifie si les questions et réponses sont bien existantes et si oui, si elles ne sont pas vides (facile de supprimer le champ required)
    - Ajout d'un type en fonction de la question pour la redirection en cas d'erreur, sans ce type on arrive sur une autre redirection et donc on a pas de message

--- PHP/app/view/admin/faq-edit.php
+++ PHP/app/view/admin/faq-edit.php
@@ -4,20 +4,20 @@
 		$faq = Faq::getFaqById($id);
 
 		if (isset($_POST['edit'])) {
-			if (isset($_POST['question']) && isset($_POST['answer'])) {
+			if (isset($_POST['question']) && !empty($_POST['question']) && isset($_POST['answer']) && !empty($_POST['answer'])) {
 				$edit = Faq::editFaq($id, $_POST['question'], $_POST['answer'], $_POST['target']);
 
 				if ($edit) {
-					$msg->success('Cette question a bien été modifié.', 'index.php?page=admin/faq-list');
+					$msg->success('Cette question a bien été modifiée.', 'index.php?page=admin/faq-list&type=' . $faq->target);
 				}
 			}
 
 			else {
-				if (!isset($_POST['question'])) {
+				if (!isset($_POST['question']) || empty($_POST['question'])) {
 					$msg->error('Veuillez entrer une question.', 'index.php?page=admin/faq-edit&id=' . $id);
 				}
 
-				if (!isset($_POST['answer'])) {
+				if (!isset($_POST['answer']) || empty($_POST['answer'])) {
 					$msg->error('Veuillez entrer une réponse.', 'index.php?page=admin/faq-edit&id=' . $id);
 				}
 			}
@@ -36,22 +36,22 @@
 					<form action="index.php?page=admin/faq-edit&amp;id=<?php echo $id; ?>" method="POST">
 
 						<div class="form-group">
-							<label for="faq-target">Pays</label>
+							<label for="faq-target">Cible</label>
 							<select name="target" id="faq-target" required="required" class="form-control">
-								<option value="" disabled>Choisissez le pays pour lequel la question est destinée</option>
+								<option value="" disabled>Choisissez le type de personne pour laquelle la question est destinée</option>
 								<option value="student"<?php if ($faq->target == 'student') {echo ' selected';} ?>>Etudiant</option>
 								<option value="company"<?php if ($faq->target == 'company') {echo ' selected';} ?>>Entreprise</option>
 							</select>
 						</div>
 
 						<div class="form-group">
-							<label for="faq-question">Questions</label>
-							<input type="text" class="form-control" id="faq-question" required="required" value="<?php echo $faq->question; ?>" name="question" placeholder="faq-question">
+							<label for="faq-question">Question</label>
+							<input type="text" class="form-control" id="faq-question" value="<?php echo $faq->question; ?>" name="question" placeholder="Votre question" required>
 						</div>
 
 						<div class="form-group">
 							<label for="faq-answer">Réponse</label>
-							<textarea class="form-control" id="faq-answer" required="required" name="answer" placeholder="faq-answer"><?php echo $faq->answer; ?></textarea>
+							<textarea class="form-control" id="faq-answer" name="answer" placeholder="Réponse de votre question" required><?php echo $faq->answer; ?></textarea>
 						</div>
 
 						<button type="submit" class="btn btn-lg btn-primary" name="edit">Éditer</button>
