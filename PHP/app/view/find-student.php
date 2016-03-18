d933edb2db9465306d473cd5003d27a66c6cb59a
ValGou <val.gougeon@hotmail.fr>
2016-03-18 20:44:10 +0100

    Retirer CV dans la la liste

--- PHP/app/view/find-student.php
+++ PHP/app/view/find-student.php
@@ -11,11 +11,11 @@
 						}
 
 						else {
-							var table = '<table class="table table-striped"><thead><tr><th>Nom</th><th>Prénom</th><th>Pays</th><th>Compétence</th><th>Email</th><th>CV</th><th>Portfolio</th><th>Date d\'inscription</th><th></th></tr></thead><tbody>';
+							var table = '<table class="table table-striped"><thead><tr><th>Nom</th><th>Prénom</th><th>Pays</th><th>Compétence</th><th>Email</th><th>Portfolio</th><th>Date d\'inscription</th><th></th></tr></thead><tbody>';
 							
 							for (i = 0; i < result.length; i++) {
 								var studentInfo = result[i];
-								table += '<tr><td>' + studentInfo.last_name + '</td><td>' + studentInfo.first_name + '</td><td>' + studentInfo.country + '</td><td>' + studentInfo.skill + '</td><td><a href="mailto:' + studentInfo.email + '">' + studentInfo.email + '</a></td><td>' + studentInfo.cv + '<td><a href="'+studentInfo.portfolio+'" target="_blank">' + studentInfo.portfolio + '</a></td><td>' + studentInfo.register_date + '</td><td><i class="fa fa-envelope"></i> <a href="mailto:' + studentInfo.email + '">Contacter</a></td><td><i class="fa fa-user"></i> <a href="index.php?page=view-profile-student&id=' + studentInfo.id + '">Voir le profil</a></td></tr>';
+								table += '<tr><td>' + studentInfo.last_name + '</td><td>' + studentInfo.first_name + '</td><td>' + studentInfo.country + '</td><td>' + studentInfo.skill + '</td><td><a href="mailto:' + studentInfo.email + '">' + studentInfo.email + '</a></td><td><a href="'+studentInfo.portfolio+'" target="_blank">' + studentInfo.portfolio + '</a></td><td>' + studentInfo.register_date + '</td><td><i class="fa fa-envelope"></i> <a href="mailto:' + studentInfo.email + '">Contacter</a></td><td><i class="fa fa-user"></i> <a href="index.php?page=view-profile-student&id=' + studentInfo.id + '">Voir le profil</a></td></tr>';
 							}
 
 							table += '</tbody></table>';
