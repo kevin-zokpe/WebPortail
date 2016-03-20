3175c7014cbf6b08424090d578b5d51345331f02
Hadrien <hadriien@live.fr>
2016-03-20 10:11:38 +0100

    Update home.php
    - Corrections W3C par rapport à une div fermée trop tôt
    - Ajout des partenaires entreprises et université, dans une mise en page cohérente avec l'intégration prochaine

--- PHP/app/view/home.php
+++ PHP/app/view/home.php
@@ -4,6 +4,10 @@
 			<div class="jumbotron" style="text-align: center;">
 				<h1>Web Portal</h1>
 				<?php
+					foreach (Partner::getPartnersList('university') as $university) {
+						echo '<div style="margin-bottom: 30px;"><img src="' . $university->logo . '" alt="' . $university->name . '" style="height: 30px;"></div>';
+					}
+
 					if (!App::isLogged()) {
 				?>
 				<p>Me connecter en tant que :</p>
@@ -11,14 +15,22 @@
 					<a class="btn btn-primary btn-lg" href="index.php?page=login&amp;type=student" role="button"><?php echo $language->translate('student'); ?></a>
 					<a class="btn btn-primary btn-lg" href="index.php?page=login&amp;type=company" role="button"><?php echo $language->translate('company'); ?></a>
 				</p>
-			</div>
-			<?php
-				}
+				<?php
+					}
 
-				else {
-					echo Student::countStudentsInternshipRequest() . ' étudiants sont en recherche de stage';
-				}
-			?>
+					else {
+						echo Student::countStudentsInternshipRequest() . ' étudiants sont en recherche de stage';
+					}
+				?>
+			</div>
 		</div>
+	</div>
+
+	<div class="row">
+		<?php
+			foreach (Partner::getPartnersList('company') as $partner) {
+				echo '<div class="col-md-2"><img src="' . $partner->logo . '" alt="' . $partner->name . '" style="height: 70px;"></div>';
+			}
+		?>
 	</div>
 </div>
