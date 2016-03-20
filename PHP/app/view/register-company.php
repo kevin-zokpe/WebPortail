4439cd4db85e04316b73974ff9909cb98de509cd
Hadrien <hadriien@live.fr>
2016-03-20 13:22:37 +0100

    Update register-company.php
    - Sécurisation des inscriptions
    - Ajout du SQL dans la classe Company

--- PHP/app/view/register-company.php
+++ PHP/app/view/register-company.php
@@ -1,137 +1,127 @@
 <?php
-	if (isset($_POST['register'])) {
+	if (!App::isLogged()) :
+		if (isset($_POST['register'])) {
+			$recaptchaClass = new Recaptcha('6Lc2cBoTAAAAAFUipDOigbn4PrJIScG6bwUqWbTQ');
 
-		$recaptchaClass = new Recaptcha('6Lc2cBoTAAAAAFUipDOigbn4PrJIScG6bwUqWbTQ');
+			if (isset($_FILES['logo'])) {
+				$my_file = basename($_FILES['logo']['name']);
+				$max_file_size = 6000000;
+				$file_size = filesize($_FILES['logo']['tmp_name']);
+				$file_ext = strrchr($_FILES['logo']['name'], '.');
+			}
 
-		if(isset($_FILES['logo'])){
-			$my_file = basename($_FILES['logo']['name']);
-			$max_file_size = 6000000;
-			$file_size = filesize($_FILES['logo']['tmp_name']);
-			$file_ext = strrchr($_FILES['logo']['name'], '.');
-		}
+			if (isset($_POST['name']) && !empty($_POST['name']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name']) &&
+	    		isset($_POST['email']) && !empty($_POST['email']) && preg_match("#^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email']) &&
+	    		isset($_POST['email-confirm']) && $_POST['email-confirm'] == $_POST['email'] &&
+	    		!Company::checkEmailExist($_POST['email']) &&
+	    	   	isset($_POST['password']) && !empty($_POST['password']) && preg_match("#^[a-zA-Z\@._-]{8}#", $_POST['password']) &&
+	    	   	isset($_POST['password-confirm']) && $_POST['password-confirm'] == $_POST['password'] &&
+	    	   	isset($_POST['country'] ) && !empty($_POST['country']) &&
+	    	   	isset($_POST['city']) && !empty($_POST['city']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['city']) &&
+	    	   	isset($_POST['description']) && preg_match("#^[a-zA-Z0-9._-]{2,128}#", $_POST['description']) &&
+	   	   		preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['website']) && 
+	    	   	isset($_POST['accept_terms']) &&
+	    	   	isset($_POST['g-recaptcha-response'])
+	    	   ) {
 
-		if (isset($_POST['name']) && !empty($_POST['name']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name']) &&
-    		isset($_POST['email']) && !empty($_POST['email']) && preg_match("#^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email']) &&
-    		isset($_POST['email-confirm']) && $_POST['email-confirm'] == $_POST['email'] &&
-    		Company::checkEmailExist($_POST['email'])==false &&
-    	   	isset($_POST['password']) && !empty($_POST['password']) && preg_match("#^[a-zA-Z\@._-]{8}#", $_POST['password']) &&
-    	   	isset($_POST['password-confirm']) && $_POST['password-confirm'] == $_POST['password'] &&
-    	   	isset($_POST['country'] ) && !empty($_POST['country']) &&
-    	   	isset($_POST['city']) && !empty($_POST['city']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['city']) &&
-    	   	isset($_POST['description']) && preg_match("#^[a-zA-Z0-9._-]{2,128}#", $_POST['description']) &&
-   	   		preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['website']) && 
-    	   	isset($_POST['accept_terms']) &&
-    	   	isset($_POST['g-recaptcha-response'])
-    	   ) {
-    	    	    
-			try {
-				PDOConnexion::setParameters('stages', 'root', 'root');
-				$db = PDOConnexion::getInstance();
-				$sql = "
-					INSERT INTO company(name, email, country, city, description, password, website, activated, register_date)
-					VALUES (:name, :email, :country, :city, :description, :password, :website, false, NOW())
-				";
-				$sth = $db->prepare($sql);
-				$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Student');
-				$sth->execute(array(
-					':name' => $_POST['name'],
-					':email' => $_POST['email'],
-					':country' => $_POST['country'],
-					':city' => $_POST['city'],
-					':description' => $_POST['description'],
-					':password' => Bcrypt::hashPassword($_POST['password']),
-					':website' => $_POST['website']
-				));
+				try {
+					$addCompany = Company::addCompany($_POST['name'], $_POST['email'], $_POST['country'], $_POST['city'], $_POST['description'], $_POST['password'], $_POST['website']);
 
-				if(isset($_FILES['logo'])){
-					if($file_ext == '.jpg' || $file_ext == '.png'){
-						if($file_size < $max_file_size){
+					if ($addCompany) {
+						if (isset($_FILES['logo'])) {
+							if ($file_ext == '.jpg' || $file_ext == '.png') {
+								if ($file_size < $max_file_size) {
+									$companyId = Company::getCompanyIDByEmail($_POST['email']);
 
-							$id_company = Company::getCompanyIDByEmail($_POST['email']);
+		          					$folder = 'uploads/companies';
+		          					
+		          					if ($file_ext == '.jpg') {
+		          						$file = $folder . '/' . $companyId->id . '.jpg';
+		          					}
 
-          					$folder = "uploads/companies";
-          					if($file_ext == '.jpg')
-        			  			$file = $folder . '/' . $id_company->id . '.jpg';
-        			  		if($file_ext == '.png')
-        			  			$file = $folder . '/' . $id_company->id . '.png';
-        			 		move_uploaded_file($_FILES['logo']['tmp_name'], $file);
+		        			  		if ($file_ext == '.png') {
+		        			  			$file = $folder . '/' . $companyId->id . '.png';
+		        			  		}
 
-        			 		PDOConnexion::setParameters('stages', 'root', 'root');
-							$dbh = PDOConnexion::getInstance();
-							$req = "UPDATE company SET logo = :logo WHERE id = :id";
-							$st = $dbh->prepare($req);
-							$st->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Company');
-							$st->execute(array(
-								':logo' => $file,
-								':id' => $id_company->id
-							));
+		        			 		move_uploaded_file($_FILES['logo']['tmp_name'], $file);
+		        			 		$editLogo = Company::editLogo($companyId->id, $file);
 
-						}
-						else{
-							$msg->error("Votre logo est trop lourd, choisissez un autre fichier",'index.php?page=register-company');
+									if ($editLogo) {
+										$msg->success('Votre inscription s\'est bien déroulée ! Votre compte doit maintenant être validé par l\'administrateur du site', 'index.php?page=home');
+									}
+								}
+
+								else {
+									$msg->error('Votre logo est trop lourd, choisissez un autre fichier', 'index.php?page=register-company');
+								}
+							}
+
+							else {
+								$msg->error('Le logo doit être au format JPG ou PNG', 'index.php?page=register-company');
+							}
 						}
 					}
-					else{
-						$msg->error("Le logo doit être au format JPG ou PNG",'index.php?page=register-company');
+
+					else {
+						$msg->error('Votre inscription a rencontré un problème. Veuillez réessayer.', 'index.php?page=register-company');
 					}
+
+					App::redirect('index.php?page=home');
+				}
+				catch(PDOException$e) {
+					echo '<p>Erreur : ' . $e->getMessage() . '</p>';
+					die();
+				}
+			}
+			else {
+				if ((!isset($_POST['name']) || empty($_POST['name'])) || 
+				(!isset($_POST['city']) || empty($_POST['city'])) ||
+				(!isset($_POST['email']) || empty($_POST['email'])) ||
+				(!isset($_POST['email-confirm']) || empty($_POST['email-confirm'])) ||
+				(!isset($_POST['password']) || empty($_POST['password'])) ||
+				(!isset($_POST['password-confirm']) || empty($_POST['password-confirm'])) ||
+				(!isset($_POST['country']) || empty($_POST['country'])) ||
+				(!isset($_POST['description']))){
+					$msg->error('Vous devez remplir tous les champs obligatoires', 'index.php?page=register-company');
 				}
 
-				App::redirect('index.php?page=home');
-			}
-			catch(PDOException$e) {
-				echo '<p>Erreur : ' . $e->getMessage() . '</p>';
-				die();
+				if (!preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name'])) {
+					$msg->error('Veuillez entrer un nom approprié', 'index.php?page=register-company');
+				}
+
+				if (!preg_match("#^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email'])) {
+					$msg->error('Veuillez entrer un email approprié', 'index.php?page=register-company');
+				}
+
+				if (Company::checkEmailExist($_POST['email'])) {
+					$msg->error('Cette adresse email est déjà utilisée', 'index.php?page=register-company');
+				}
+
+				if (!preg_match("#^[a-zA-Z\@._-]{8}#", $_POST['password'])) {
+					$msg->error('Veuillez entrer un mot de passe approprié', 'index.php?page=register-company');
+				}
+
+				if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['website'])){
+					$msg->error('Veuillez entrer une adresse web appropriée', 'index.php?page=register-company');
+				}
+
+				if ($_POST['email'] != $_POST['email-confirm']) {
+					$msg->error('L\'adresse email doit correspondre', 'index.php?page=register-company');
+				}
+
+				if ($_POST['password'] != $_POST['password-confirm']) {
+					$msg->error('Le mot de passe doit correspondre', 'index.php?page=register-company');
+				}
+
+				if (!isset($_POST['accept_terms'])) {
+					$msg->error('Vous devez accepter les conditions d\'utilisation', 'index.php?page=register-company');
+				}
+
+				if (!isset($_POST['g-recaptcha-response'])) {
+					$msg->error('Vous devez confirmer que vous n\'êtes pas un robot', 'index.php?page=register-company');
+				}
 			}
 		}
-		else {
-			if ((!isset($_POST['name']) || empty($_POST['name'])) || 
-			(!isset($_POST['city']) || empty($_POST['city'])) ||
-			(!isset($_POST['email']) || empty($_POST['email'])) ||
-			(!isset($_POST['email-confirm']) || empty($_POST['email-confirm'])) ||
-			(!isset($_POST['password']) || empty($_POST['password'])) ||
-			(!isset($_POST['password-confirm']) || empty($_POST['password-confirm'])) ||
-			(!isset($_POST['country']) || empty($_POST['country'])) ||
-			(!isset($_POST['description']))){
-				$msg->error('Vous devez remplir tous les champs obligatoires','index.php?page=register-company');
-			}
-
-			if (!preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name'])){
-				$msg->error("Veuillez entrer un nom approprié",'index.php?page=register-company');
-			}
-
-			if (!preg_match("#^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email'])){
-				$msg->error("Veuillez entrer un email approprié",'index.php?page=register-company');
-			}
-
-			if (Company::checkEmailExist($_POST['email'])==true){
-				$msg->error("Cette adresse email est déjà utilisée",'index.php?page=register-company');
-			}
-
-			if (!preg_match("#^[a-zA-Z\@._-]{8}#", $_POST['password'])){
-				$msg->error("Veuillez entrer un mot de passe approprié",'index.php?page=register-company');
-			}
-
-			if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST['website'])){
-				$msg->error("Veuillez entrer une adresse web appropriée",'index.php?page=register-company');
-			}
-
-			if ($_POST['email']!=$_POST['email-confirm']){
-				$msg->error("L'adresse email doit correspondre",'index.php?page=register-company');
-			}
-
-			if ($_POST['password']!=$_POST['password-confirm']){
-				$msg->error("Le mot de passe doit correspondre",'index.php?page=register-company');
-			}
-
-			if (!isset($_POST['accept_terms'])){
-				$msg->error("Vous devez accepter les conditions d'utilisation",'index.php?page=register-company');
-			}
-
-			if (!isset($_POST['g-recaptcha-response'])){
-				$msg->error("Vous devez confirmer que vous n'êtes pas un robot",'index.php?page=register-company');
-			}
-		}
-	}
 ?>
 <style type="text/css">
 	form .row {
@@ -237,7 +227,7 @@
 				<div class="row">
 					<div class="col-md-12">
 						<label for="signup-terms">Code de vérification</label>
-						<div class="g-recaptcha" data-sitekey="6Lc2cBoTAAAAAF9TTSA9CRMbg-jss8X-CmDy_eXI"></div>
+						<div class="g-recaptcha" data-sitekey="<?php echo Settings::getRecaptchaKey('public'); ?>"></div>
 						<p class="help-block">Vérifiez que vous êtes un humain</p>
 					</div>
 				</div>
@@ -251,3 +241,8 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/jquery.form-validator.min.js"></script>
 <script src="https://www.google.com/recaptcha/api.js"></script>
+<?php
+	else:
+		App::redirect('index.php?page=home');
+	endif;
+?>
