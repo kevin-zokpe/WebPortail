d2c3ed51030f734d1223b998e4867d67a82a1d18
ValGou <val.gougeon@hotmail.fr>
2016-03-22 22:10:50 +0100

    Update find-internship.php

--- PHP/app/view/find-internship.php
+++ PHP/app/view/find-internship.php
@@ -1,22 +1,21 @@
-<?php if (App::isCompany()) : ?>
+<?php if (App::isStudent()) : ?>
 	<script type="text/javascript">
 		$(document).ready(function() {
 			$('select[name="skill"]').change(function() {
 				var selectedSkill = $(this).val();
-				var country = $(this).attr('data-country');
 
 				$.getJSON(
-					"public/webservice/students.php", {"skill": selectedSkill, "country": country}, function(result) {
+					"public/webservice/internships.php", {"skill": selectedSkill}, function(result) {
 						if (result.no_result) {
-							$('#results').html('Aucun étudiant.');
+							$('#results').html('Aucun stage disponible dans ce domaine');
 						}
 
 						else {
-							var table = '<table class="table table-striped"><thead><tr><th>Nom</th><th>Prénom</th><th>Pays</th><th>Compétence</th><th>Email</th><th>Portfolio</th><th>Date d\'inscription</th><th></th></tr></thead><tbody>';
+							var table = '<table class="table table-striped"><thead><tr><th>Entrepise</th><th>Poste</th><th>Mission</th><th>Adresse</th><th>Ville</th><th>Code postal</th><th></th><th></th></tr></thead><tbody>';
 							
 							for (i = 0; i < result.length; i++) {
-								var studentInfo = result[i];
-								table += '<tr><td>' + studentInfo.last_name + '</td><td>' + studentInfo.first_name + '</td><td>' + studentInfo.country + '</td><td>' + studentInfo.skill + '</td><td><a href="mailto:' + studentInfo.email + '">' + studentInfo.email + '</a></td><td><a href="'+studentInfo.portfolio+'" target="_blank">' + studentInfo.portfolio + '</a></td><td>' + studentInfo.register_date + '</td><td><i class="fa fa-envelope"></i> <a href="mailto:' + studentInfo.email + '">Contacter</a></td><td><i class="fa fa-user"></i> <a href="index.php?page=view-profile-student&id=' + studentInfo.id + '">Voir le profil</a></td></tr>';
+								var internshipInfo = result[i];
+								table += '<tr><td><a href="index.php?page=view-profile-company&id=' + internshipInfo.id_company + '">' + internshipInfo.company + '</a></td><td>' + internshipInfo.name + '</td><td>' + internshipInfo.description + '</td><td>' + internshipInfo.address + '</td><td>' + internshipInfo.city + '</td><td>' + internshipInfo.zip_code + '</td>' + '<td><i class="fa fa-envelope"></i> <a href="mailto:' + internshipInfo.email + '">Contacter</a></td>' + '<td><i class="fa fa-file-text"></i> <a href="index.php?page=internship-info&id=' + internshipInfo.id + '">Voir la fiche</a></td>';
 							}
 
 							table += '</tbody></table>';
@@ -32,12 +31,13 @@
 			<div class="col-md-12">
 				<div class="page-header">
 					<h1>
-						Rechercher des étudiants
+						Rechercher un stage
 						<form action="index.php?page=find-student" method="POST" class="pull-right">
 							<select name="skill" class="form-control" data-country="<?php echo $_SESSION['country']; ?>">
 								<option value="" disabled selected>Domaine de compétence</option>
 								<?php
 									foreach (Skill::getSkillsList() as $skill) {
+										
 										echo '<option value="' . $skill->id . '">' . $skill->name . '</option>';
 									}
 								?>
