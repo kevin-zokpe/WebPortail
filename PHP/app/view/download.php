6273ab0008f7d659ccac7b90aec2236166ca4742
ValGou <val.gougeon@hotmail.fr>
2016-03-23 00:24:06 +0100

    Update download.php

--- PHP/app/view/download.php
+++ PHP/app/view/download.php
@@ -2,15 +2,10 @@
 	$id = $_GET['id'];
 	$student = Student::getStudentById($id);
 	$new_file = $student->first_name . '_' . $student->last_name . '_CV.pdf';
-	 
-	// lecture binaire du fichier
-	$content_file = file_get_contents(BASE_LINK . '/public/uploads/cv/' . $id . '.pdf');
-	 
+	$file = BASE_LINK . '/public/uploads/cv/' . $id . '.pdf';
 	// Entetes HTTP pour l'envoi
-	header( 'Content-type: application/pdf'); 
-	header( 'Content-length: ' . filesize(BASE_LINK . '/public/uploads/cv/' . $id . '.pdf'));
-	header( 'Content-disposition: attachment; filename="'.$new_file.'"');
+	header('Content-Type: application/pdf'); 
+	header('Content-Disposition: attachment; filename="' . $new_file . '"');
+	readfile($file);
 	 
-	// envoi au navigateur
-	echo $content_file;
 ?>
