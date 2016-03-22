08f7c9a50054e9a2b8c7fa3a7a726d7fd99d37f3
ValGou <val.gougeon@hotmail.fr>
2016-03-22 19:25:03 +0100

    Update faq.php

--- PHP/app/view/faq.php
+++ PHP/app/view/faq.php
@@ -1,6 +1,9 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
+		<?php
+			if($language->getCurrentLanguage()['code']=='fr'){
+		?>
 			<h2>FAQ Etudiant</h2>
 			<?php
 				foreach(Faq::getStudentsFaq() as $faq) {
@@ -8,11 +11,11 @@
 						<br />
 						<div class="row">
 							<div class="col-md-offset-2 col-md-8">
-								<p><b>' . $faq->question . '</b></p>
+								<p><b>' . $faq->question_fr . '</b></p>
 							</div>
 						</div>
 						<div class="row">
-							<div class="col-md-offset-2 col-md-8">' . $faq->answer . '</div>
+							<div class="col-md-offset-2 col-md-8">' . $faq->answer_fr . '</div>
 						</div>
 					';
 				}
@@ -25,14 +28,52 @@
 						<br />
 						<div class="row">
 							<div class="col-md-offset-2 col-md-8">
-								<p><b>' . $faq->question . '</b></p>
+								<p><b>' . $faq->question_fr . '</b></p>
 							</div>
 						</div>
 						<div class="row">
-							<div class="col-md-offset-2 col-md-8">' . $faq->answer . '</div>
+							<div class="col-md-offset-2 col-md-8">' . $faq->answer_fr . '</div>
 						</div>
 					';
 				}
+			}
+			else{
+
+			?>
+			<h2>Student FAQ</h2>
+			<?php
+				foreach(Faq::getStudentsFaq() as $faq) {
+					echo '
+						<br />
+						<div class="row">
+							<div class="col-md-offset-2 col-md-8">
+								<p><b>' . $faq->question_en . '</b></p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-offset-2 col-md-8">' . $faq->answer_en . '</div>
+						</div>
+					';
+				}
+			?>
+
+			<h2>Company FAQ</h2>
+			<?php
+				foreach(Faq::getCompaniesFaq() as $faq) {
+					echo '
+						<br />
+						<div class="row">
+							<div class="col-md-offset-2 col-md-8">
+								<p><b>' . $faq->question_en . '</b></p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-offset-2 col-md-8">' . $faq->answer_en . '</div>
+						</div>
+					';
+				}
+			}
+			
 			?>
 		</div>
 	</div>
