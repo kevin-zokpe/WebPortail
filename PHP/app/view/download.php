bb1a4c7b72c38f9cfbc2ec90f8e056aaae1c9efd
Hadrien <hadriien@live.fr>
2016-03-24 20:10:01 +0100

    Update download.php
    Correction du bug des PDF

--- PHP/app/view/download.php
+++ PHP/app/view/download.php
@@ -2,10 +2,24 @@
 	$id = $_GET['id'];
 	$student = Student::getStudentById($id);
 	$new_file = $student->first_name . '_' . $student->last_name . '_CV.pdf';
-	$file = BASE_LINK . '/public/uploads/cv/' . $id . '.pdf';
-	// Entetes HTTP pour l'envoi
-	header('Content-Type: application/pdf'); 
-	header('Content-Disposition: attachment; filename="' . $new_file . '"');
-	readfile($file);
-	 
+	$file = 'uploads/cv/' . $id . '.pdf';
+
+	if (file_exists($file)) {
+	    header('Content-Description: File Transfer');
+	    header('Content-Type: application/pdf');
+	    header("Content-Type: application/force-download");
+	    header('Content-Disposition: attachment; filename=' . $new_file);
+	    header('Expires: 0');
+	    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	    header('Pragma: public');
+	    header('Content-Length: ' . filesize($file));
+	    ob_clean();
+	    flush();
+	    readfile($file);
+	    exit;
+	}
+
+	else {
+		$msg->error('Le CV est introuvable.', 'index.php?page=view-profile-student&id=' . $id);
+	}
 ?>
