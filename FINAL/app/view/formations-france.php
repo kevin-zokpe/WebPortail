0f5bd1037889b74e26c55d7c466d312383a27c55
ValGou <val.gougeon@hotmail.fr>
2016-04-01 09:57:59 +0200

    Update formations-france.php

--- FINAL/app/view/formations-france.php
+++ FINAL/app/view/formations-france.php
@@ -10,12 +10,11 @@
 <div id="main-content" class="section-content">
     <div class="container">
         <div class="row">
-            <div class="col-md-offset-1 col-md-8">Les étudiants Français que vous trouverez sur Webportal sont ici de prestigieuses universités Françaises. <br/>Ici sont détaillés l'ensemble des cours dispensés à vos futurs stagiaires Français !</div><br /><br />
-            <h3>IUT Cherbourg Manche - <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/" target="_blank"> Diplômes Universitaires de Technologie</a></h3>
+            <div class="col-md-offset-2 col-md-8">Les étudiants Français que vous trouverez sur Webportal sont ici de prestigieuses universités Françaises. <br/>Ici sont détaillés l'ensemble des cours dispensés à vos futurs stagiaires Français !</div><br /><br />
+            <div class="col-md-offset-2 col-md-8" style="margin-bottom:50px;margin-top:30px;"><h3>IUT Cherbourg Manche - <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/" target="_blank"> Diplômes Universitaires de Technologie</a></h3></div>
 
             <div class="col-xs-6 col-md-4">
                 <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/mmi-multimedia-et-internet-/" target="_blank" class="thumbnail">
-                    <img src="public/img/formations-france/mmi.jpg" alt="MMI">
                     <div class="caption">
                         <h3>Métiers du Mutlimédia et de l'Internet</h3>
                     </div>
@@ -24,16 +23,14 @@
 
             <div class="col-xs-6 col-md-4">
                 <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/gte-energie-environnement-/" target="_blank" class="thumbnail">
-                    <img src="public/img/formations-france/gte.jpg" alt="GTE">
                     <div class="caption">
-                        <h3>Génie Thermique et Énergie</h3>
+                        <h3>Génie Thermique et Énergie<br/>-</h3>
                     </div>
                 </a>
             </div>
 
             <div class="col-xs-6 col-md-4">
                 <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/gea-gestion-/" target="_blank" class="thumbnail">
-                    <img src="public/img/formations-france/gea.jpg" alt="GEA">
                     <div class="caption">
                         <h3>Gestion des Entreprises et des Administrations</h3>
                     </div>
@@ -42,7 +39,6 @@
 
             <div class="col-xs-6 col-md-4">
                 <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/tc-commerce-/" target="_blank" class="thumbnail">
-                    <img src="public/img/formations-france/tc.jpg" alt="TC">
                     <div class="caption">
                         <h3>Techniques de Commercialisation</h3> 
                     </div>
@@ -51,7 +47,6 @@
 
             <div class="col-xs-6 col-md-4">
                 <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/gim-ingenierie-developpement-durable-/" target="_blank" class="thumbnail">
-                    <img src="public/img/formations-france/gim.jpg" alt="GIM">
                     <div class="caption">
                         <h3>Génie Industriel et Maintenance</h3>
                     </div>
@@ -60,7 +55,6 @@
 
             <div class="col-xs-6 col-md-4">
                 <a href="http://iutcherbourgmanche.unicaen.fr/d-u-t-/geii-reseaux-automatismes-nouvelles-technologies-/" target="_blank" class="thumbnail">
-                    <img src="public/img/formations-france/geii.jpg" alt="GEII">
                     <div class="caption">
                         <h3>Génie Electrique et Informatique Industrielle</h3>
                     </div>
